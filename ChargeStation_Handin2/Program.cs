1d6cab9469c77ef7c4fb2003d1e38d7a25635a32
Nanna Primdal <201811526@post.au.dk>
2021-03-17 09:31:22 +0100

    Add project files.

---
+++ ChargeStation_Handin2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ChargeStation_Handin2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
