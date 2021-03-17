8955b9cd51a3f178ee80f98e92ca9d6781c2ffc8
Nanna Primdal <201811526@post.au.dk>
2021-03-17 10:10:39 +0100

    Log tilføjet i file

---
+++ ChargeStation_Handin2/File/ILogFile.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChargeStation_Handin2
+{
+   
+        public interface ILogFile
+        {
+            public void LogDoorLocked(int id);
+            public void LogDoorUnlocked(int id);
+        }
+    
+}
