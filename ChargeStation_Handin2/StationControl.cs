279f00e5235856367039f51ba425be8511388bb7
Nanna Primdal <201811526@post.au.dk>
2021-03-17 10:02:10 +0100

    Connected ændret

--- ChargeStation_Handin2/StationControl.cs
+++ ChargeStation_Handin2/StationControl.cs
@@ -34,7 +34,7 @@ namespace ChargeStation_Handin2
             {
                 case LadeskabState.Available:
                     // Check for ladeforbindelse
-                    if (_charger.Connected)
+                    if (_charger.IsConnected())
                     {
                         _door.LockDoor();
                         _charger.StartCharge();
