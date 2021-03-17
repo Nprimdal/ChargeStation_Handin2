3026c58813816dbc2a19c8eb4a0f9603999a6bd1
Nanna Primdal <201811526@post.au.dk>
2021-03-17 09:56:37 +0100

    ChargeControl tilføjet

---
+++ ChargeStation_Handin2/Charge/ChargeControl.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ChargeStation_Handin2
+{
+    public class ChargeControl : IChargeControl
+    {
+        public bool IsConnected()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void StartCharge()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void StopCharge()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void HandleCurrentEventChanged(object s, CurrentEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- ChargeStation_Handin2/Charge/IChargeControl.cs
+++ ChargeStation_Handin2/Charge/IChargeControl.cs
@@ -1,10 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Text;
 
 namespace ChargeStation_Handin2
 {
     public interface IChargeControl
     {
+        bool IsConnected();
+        void StartCharge();
+        void StopCharge();
+        void HandleCurrentEventChanged(object s, CurrentEventArgs e);
+
     }
 }
